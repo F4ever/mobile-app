3994028a37b91a66bce1df844e86cdf6623837a9
Raman <roman.s@p2p.org>
2021-10-09 23:20:21 +0300

    Setup menu

--- App.js
+++ App.js
@@ -3,17 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./components/Home";
 import GameSettings from "./components/GameSettings";
+import Game from "./components/Game";
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Menu">
-        <Stack.Screen name="Menu" component={Home} />
-        <Stack.Screen name="GameSettings" component={GameSettings} options={{ title: 'Game difficulty' }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Game">
+                <Stack.Screen name="Menu" component={Home} />
+                <Stack.Screen name="GameSettings" component={GameSettings} options={{ title: "Game difficulty" }}/>
+                <Stack.Screen name="Game" component={Game} options={{ title: "Super Game"}}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
